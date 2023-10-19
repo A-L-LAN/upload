cfbeccf4c1b23e69d2b0b32643e80af886b4d273
A-L-LAN <144988674+A-L-LAN@users.noreply.github.com>
2023-10-19 16:31:30 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,29 @@
+# server.py 
+
+from flask import Flask, request, send_file
+import gridfs
+from pymongo import MongoClient
+
+app = Flask(_name_)
+
+# Connect to MongoDB Atlas cluster 
+client = MongoClient("mongodb+srv://<allankipruto4th>:<allan28>@mycluster.mongodb.net/images?retryWrites=true&w=majority")
+db = client.images
+fs = gridfs.GridFS(db)
+
+@app.route('/upload', methods=['POST']) 
+def upload_image():
+  if request.files:
+    image = request.files['image']
+    image_id = fs.put(image)
+    print('Image uploaded with ID:', image_id)
+    
+    return image_id
+
+@app.route('/image/<image_id>')
+def get_image(image_id):
+  file = fs.get(image_id)
+  return send_file(file, mimetype='image/jpeg')
+  
+if _name_ == '_main_':
+  app.run(debug=True)
